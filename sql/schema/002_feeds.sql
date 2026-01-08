4ed8dffaa69bb43d16264d398245a42e6481d6e3
Zach <zlaubacher@gmail.com>
2026-01-07 23:34:07 -0500

    created and generated a feeds table

---
+++ sql/schema/002_feeds.sql
@@ -0,0 +1,15 @@
+-- +goose up
+CREATE TABLE feeds (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    name TEXT NOT NULL,
+    url TEXT UNIQUE NOT NULL,
+    user_id UUID NOT NULL,
+    FOREIGN KEY(user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE
+);
+
+-- +goose down
+DROP TABLE IF EXISTS feeds;
